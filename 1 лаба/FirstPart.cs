a468d6423798463e7594574fcc089b57e34039cf
wizoomm <115590378+wizoomm@users.noreply.github.com>
2023-12-07 01:10:14 +0300

    Update FirstPart.cs

--- 1 лаба/FirstPart.cs
+++ 1 лаба/FirstPart.cs
@@ -22,7 +22,6 @@ namespace laba1f
                 array[i] = random.Next(-10, 10);
             }
         }
-        
         public IReadOnlyList<int> Vector
         {
             get
@@ -30,7 +29,6 @@ namespace laba1f
                 return array;
             }
         }
-        
         public int GetCountNegative()
         {
             int count = 0;
@@ -42,16 +40,11 @@ namespace laba1f
                     count++;
                 }
             }
-
             return count;
         }
-        
-        
         public int SumAfterMinModule()
         {
-             int minIndex = 0;
-
-            
+            int minIndex = 0;
             for (int i = 1; i < array.Length; i++)
             {
                 if (Math.Abs(array[i]) < Math.Abs(array[minIndex]))
@@ -59,20 +52,14 @@ namespace laba1f
                     minIndex = i;
                 }
             }
-            
             int sum = 0;
-
             
             for (int i = minIndex + 1; i < array.Length; i++)
             {
                 sum += Math.Abs(array[i]);
             }
-
             return sum;
         }
-
-        
-        
         public void ReplacementNegativeElements()
         {
             for (int i = 0; i < array.Length; i++)
@@ -82,15 +69,11 @@ namespace laba1f
                     // замена отрицательного элемента его квадратом
                     array[i] = (int)Math.Pow(array[i], 2); 
                 }
-            }
-            
+            } 
         }
         public void SortByAscending()
         {
             Array.Sort(array);
-        }
-
-
-        
+        }   
     }
 }
