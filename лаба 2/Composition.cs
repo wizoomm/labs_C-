7d1855ff9ea32d8198a51738943f392c75b326b6
wizoomm <115590378+wizoomm@users.noreply.github.com>
2024-01-08 22:24:56 +0300

    Add files via upload

---
+++ лаба 2/Composition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labrab2
+{
+    internal class Composition
+    {
+        private readonly string name;
+        private readonly string composer;
+
+        public Composition(string name, string composer)
+        {
+            this.name = name;
+            this.composer = composer;
+        }
+
+        public bool SearchMatches(string query)
+        {
+            return name == query || composer == query;
+        }
+
+        public new string ToString()
+        {
+            return composer + " - " + name;
+        }
+    }
+}
